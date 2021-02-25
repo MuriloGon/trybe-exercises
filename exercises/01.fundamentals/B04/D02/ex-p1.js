ae10a77cecc10a2b72d8694b7ffcdb3f54f82c50
Murilo Gonçalves <murilo.gon@live.com>
2021-02-25 13:54:29 -0300

    feat(item 02): :rocket: use for loop to sum all numbers in an array

--- exercises/01.fundamentals/B04/D02/ex-p1.js
+++ exercises/01.fundamentals/B04/D02/ex-p1.js
@@ -4,3 +4,8 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 console.log(`\n== Item01 ==`)
 for (let number of numbers) console.log(number);
 
+// Item 02
+console.log(`\n== Item02 ==`)
+let sumResult = 0;
+for (let number of numbers) sumResult += number;
+console.log("The total array sum is: ", sumResult)
