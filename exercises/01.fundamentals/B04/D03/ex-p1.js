603c9007b9f6877ea30077e067ad9eb99a2c9647
Murilo Gonçalves <murilo.gon@live.com>
2021-02-26 15:59:58 -0300

    feat(item 02): :rocket: add crescent left-side triangle

--- exercises/01.fundamentals/B04/D03/ex-p1.js
+++ exercises/01.fundamentals/B04/D03/ex-p1.js
@@ -7,6 +7,12 @@ function isInt(value) {
     return (x | 0) === x;
 }
 
+function fillWithChar(char, times) {
+    let output = ``;
+    for (let x = 1; x <= times; x++) output += `${char}`;
+    return output;
+}
+
 // Item 01
 console.log(`\n== Item01 ==`)
 function square(n) {
@@ -17,3 +23,18 @@ function square(n) {
     } else { return null; };
 }
 square(5);
+
+// Item 02
+console.log(`\n== Item02 ==`)
+function triangle(n) {
+    if (isInt != false) {
+        let str = "";
+        for (let x = 1; x <= n; x++) {
+            let chars = x;
+            str = fillWithChar('*', chars)
+            console.log(str)
+        };
+    } else { return null; };
+}
+triangle(5)
+
