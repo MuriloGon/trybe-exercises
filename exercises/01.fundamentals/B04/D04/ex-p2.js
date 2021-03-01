ea7bd36f15262ce4c538b2160c722647527deb08
Murilo Gonçalves <murilo.gon@live.com>
2021-03-01 16:00:05 -0300

    feat(item 02): :rocket: :rocket: add exercise part2 - item 02 - max index and num

--- exercises/01.fundamentals/B04/D04/ex-p2.js
+++ exercises/01.fundamentals/B04/D04/ex-p2.js
@@ -14,3 +14,22 @@ function checkPalindrome(str) {
 }
 console.log('arara -> ', checkPalindrome('arara'))
 console.log('desenvolvimento -> ', checkPalindrome('desenvolvimento'))
+
+// Item 02
+console.log(`\n== Item02 ==`);
+let arr = [2, 3, 6, 7, 10, 1];
+function maxIndex(array) {
+    let max = {
+        num: array[0],
+        index: 0
+    }
+    for (let indexArr in array) {
+        if (array[indexArr] > max.num) {
+            max.index = indexArr;
+            max.num = array[indexArr];
+        }
+    }
+    return max
+}
+console.log('index -> ', minIndex(arr).index)
+console.log('num -> ', minIndex(arr).num)
