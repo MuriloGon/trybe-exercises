320e9fc7971d46013433730c3b13b8e3313af442
Murilo Gonçalves <murilo.gon@live.com>
2021-03-01 14:41:46 -0300

    feat(item 01): :rocket: add exercise part1 - item 01 - welcome message

---
+++ exercises/01.fundamentals/B04/D04/ex-p1.js
@@ -0,0 +1,10 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald'
+};
+
+// Item 01
+console.log(`\n== Item01 ==`);
+console.log('Welcome, ' + info.personagem);
+console.log('Welcome, ' + info["personagem"]);
