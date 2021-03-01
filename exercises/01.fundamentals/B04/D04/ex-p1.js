d47c7934c090e1952b76b12c3facbeb3931a9b1c
Murilo Gonçalves <murilo.gon@live.com>
2021-03-01 14:58:37 -0300

    feat(item 03): :rocket: add exercise part1 - item 03 - get all keys with for/in

--- exercises/01.fundamentals/B04/D04/ex-p1.js
+++ exercises/01.fundamentals/B04/D04/ex-p1.js
@@ -15,3 +15,7 @@ console.log(Object.keys(info))
 info.recorrente = 'Sim';
 console.log(Object.keys(info))
 console.log('recorrente:', info.recorrente)
+
+// Item 03
+console.log(`\n== Item03 ==`);
+for(let datum in info) console.log(datum);
