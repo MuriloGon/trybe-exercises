62dbe9d1d9982794139d1beff0de4bacddb2a893
Murilo Gonçalves <murilo.gon@live.com>
2021-03-01 22:59:03 -0300

    feat(item 05): :rocket: :rocket: add exercise part2 - item 05 - integer mos repeated

--- exercises/01.fundamentals/B04/D04/ex-p2.js
+++ exercises/01.fundamentals/B04/D04/ex-p2.js
@@ -60,10 +60,45 @@ let names = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
 function maxChars(array) {
     let indexMaxChars = 0;
     for (let name of names) {
-        if (name.length > names[indexMaxChars].length) 
+        if (name.length > names[indexMaxChars].length)
             indexMaxChars = array.findIndex(x => x == name);
     }
     console.log(indexMaxChars)
     return array[indexMaxChars]
 }
 console.log(maxChars(names))
+
+// Item 05
+console.log(`\n== Item05 ==`);
+let arr3 = [2, 3, 2, 5, 8, 2, 3, 5, 5];
+
+function repeatedNum(array) {
+    let objOutput = new Object();
+    // Populate an object taking the unique numbers as keys and sum up +1 when repeated
+    for (let num of array) {
+        if (objOutput[num.toString()] == undefined)
+            objOutput[num.toString()] = 0;
+        objOutput[num.toString()] += 1;
+    }
+
+    // bubblesorting algorithm to sort an array with the previous object based on the pair (key, value)
+    // the algorith is sorting in crescent way based on the repeated value of the key
+    let pair = Object.entries(objOutput);
+    for (let n1 = 1; n1 < pair.length; n1 += 1) {
+        for (let n0 = 0; n0 < pair.length; n0 += 1) {
+            if (pair[n1][1] < pair[n0][1]) {
+                let position = pair[n1];
+                pair[n1] = pair[n0]
+                pair[n0] = position
+            }
+        }
+    }
+
+    // TODO(murilo.gon@live.com): fix the bug that happens when there're
+    // two or more number repeatead in the same amount
+
+    // return the last item (most repeated number)
+    // (0, 1) -> (key, value) -> (number-repeated, times-repeated)
+    return Number(pair[pair.length-1][0])
+}
+console.log(repeatedNum(arr3))
