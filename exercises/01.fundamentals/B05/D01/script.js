15417f30c724344bd4bfdeb71c7282fd48d89862
Murilo Gonçalves <murilo.gon@live.com>
2021-03-03 15:14:20 -0300

    feat(item 02): :rocket: add function that changes square's background-color

--- exercises/01.fundamentals/B05/D01/script.js
+++ exercises/01.fundamentals/B05/D01/script.js
@@ -8,3 +8,16 @@ function mySelfPTags(text) {
 setTimeout(() => {
     mySelfPTags("Trabalhando, ganhando bem e viajando pelo mundo");
 }, 500);
+
+
+//item02
+function changeSquareBgColor(className, color) {
+    let squares = document.getElementsByClassName(className);;
+    for (let square of squares) {
+        square.style.backgroundColor = color;
+    }
+}
+setTimeout(() => {
+    changeSquareBgColor('main-content', 'rgb(76,164,109)');
+}, 1000);
+
