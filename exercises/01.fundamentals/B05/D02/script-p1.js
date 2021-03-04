1763b31298c283f0d639e08aec64690aa265f1ac
Murilo Gonçalves <murilo.gon@live.com>
2021-03-04 18:48:09 -0300

    refactor: :recycle: change arguments for consistency

--- exercises/01.fundamentals/B05/D02/script-p1.js
+++ exercises/01.fundamentals/B05/D02/script-p1.js
@@ -1,18 +1,18 @@
 let body = document.querySelector('body');
 
 //item 01
-function addH1(element, innerText) {
+function addH1(parentElement, innerText) {
     let h1 = document.createElement('h1');
     h1.innerText = innerText;
-    element.append(h1);
+    parentElement.append(h1);
 }
 addH1(body, 'Exercise 5.2 - JavaScript DOM');
 
 //item 02
-function addDiv(elementParent, classList) {
+function addDiv(parentElement, classList) {
     let div = document.createElement('div');
     div.classList.add(classList);
-    elementParent.append(div);
+    parentElement.append(div);
     return div;
 }
 let divMainContent = addDiv(body, ['main-content']);
