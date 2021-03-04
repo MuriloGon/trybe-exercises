b3811bc423bdc190a719048a2b1cf0804d1fe127
Murilo Gonçalves <murilo.gon@live.com>
2021-03-04 18:29:20 -0300

    feat(exercise p1-02): :rocket: add a customizable function  that adds a div to a parente element

---
+++ exercises/01.fundamentals/B05/D02/script-p1.js
@@ -0,0 +1,18 @@
+let body = document.querySelector('body');
+
+//item 01
+function addH1(element, innerText) {
+    let h1 = document.createElement('h1');
+    h1.innerText = innerText;
+    element.append(h1);
+}
+addH1(body, 'Exercise 5.2 - JavaScript DOM');
+
+//item 02
+function addDiv(elementParent, classList) {
+    let div = document.createElement('div');
+    div.classList.add(classList);
+    elementParent.append(div);
+    return div;
+}
+let divMainContent = addDiv(body, ['main-content']);
