18e6cb828522d4073aaa5235dd1154793e18241e
Murilo Gonçalves <murilo.gon@live.com>
2021-03-05 18:33:21 -0300

    feat: :seedling: add exercises base files

---
+++ exercises/01.fundamentals/B05/D03/script.js
@@ -0,0 +1,14 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+
+    for (let index = 0; index < weekDays.length; index += 1) {
+        const days = weekDays[index];
+        const dayListItem = document.createElement('li');
+        dayListItem.innerHTML = days;
+
+        weekDaysList.appendChild(dayListItem);
+    };
+};
+
+createDaysOfTheWeek();
