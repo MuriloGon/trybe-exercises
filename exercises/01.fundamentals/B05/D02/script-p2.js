1a455a96ccec7361a640f4bd41130d087eefa675
Murilo Gonçalves <murilo.gon@live.com>
2021-03-04 21:20:30 -0300

    feat(exercise p2-03): :rocket: remove divLeftContent element p1-05

--- exercises/01.fundamentals/B05/D02/script-p2.js
+++ exercises/01.fundamentals/B05/D02/script-p2.js
@@ -4,3 +4,6 @@ mainH1.className = 'title'
 //item 02
 let allH3 = document.getElementsByTagName('h3');
 for (let h3 of allH3) h3.className = 'description';
+
+//item 03
+divMainContent.removeChild(divLeftContent);
