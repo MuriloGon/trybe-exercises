c8b29fb6caad71a9f5efc468207d4be97c7dedaf
Murilo Gonçalves <murilo.gon@live.com>
2021-03-23 16:40:00 -0300

    feat(part01-ex01): :rocket: add function with arr-func and literals

--- exercises/01.fundamentals/B07_js-es6-and-unitary-tests/D01_let-const-arrw-literals/script.js
+++ exercises/01.fundamentals/B07_js-es6-and-unitary-tests/D01_let-const-arrw-literals/script.js
@@ -1,3 +1,18 @@
 /* PART I */
+//EX01
+const testingScope = (escopo) => {
+  if (escopo === true) {
+    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+  console.log(`${ifScope} o que estou fazendo aqui ? :O`); // Se necessário esta linha pode ser removida.
+}
+
+testingScope(true);
+
 
 /* PART II */
