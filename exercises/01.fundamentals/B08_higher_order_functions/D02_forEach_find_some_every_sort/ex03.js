d1428ef34744aec1f478a2565244ed400198f73f
Murilo Gonçalves <murilo.gon@live.com>
2021-03-30 15:06:42 -0300

    feat(ex03): 🚀 use find method

--- exercises/01.fundamentals/B08_higher_order_functions/D02_forEach_find_some_every_sort/ex03.js
+++ exercises/01.fundamentals/B08_higher_order_functions/D02_forEach_find_some_every_sort/ex03.js
@@ -75,7 +75,7 @@ const expectedResult = {
 };
 
 function getNamedBook() {
-  // escreva seu código aqui
+  return books.find(book => book.name.length === 26);
 }
 
 assert.deepStrictEqual(getNamedBook(), expectedResult);
