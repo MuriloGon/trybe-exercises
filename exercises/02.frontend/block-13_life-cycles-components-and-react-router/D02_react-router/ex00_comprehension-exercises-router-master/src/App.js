c835bbada204d9ffc20e7ae1e9b85b86e7457a81
Murilo Gonçalves <murilo.gon@live.com>
2021-04-03 14:57:52 -0300

    feat(ex00-req13): :rocket: add scritaccess route to app component

--- exercises/02.frontend/block-13_life-cycles-components-and-react-router/D02_react-router/ex00_comprehension-exercises-router-master/src/App.js
+++ exercises/02.frontend/block-13_life-cycles-components-and-react-router/D02_react-router/ex00_comprehension-exercises-router-master/src/App.js
@@ -3,18 +3,24 @@ import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
 import Home from './components/Home';
 import Users from './components/Users';
 import About from './components/About';
+import StrictAccess from './StrictAcess';
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <nav>
-          <Link to='/'><li>Home</li></Link>
-          <Link to='/about'><li>About</li></Link>
-          <Link to='/users'><li>Users</li></Link>
-          <Link to='/strict-access'><li>Strict Access</li></Link>
+          <ul>
+            <Link to='/'><li>Home</li></Link>
+            <Link to='/about'><li>About</li></Link>
+            <Link to='/users'><li>Users</li></Link>
+            <Link to='/strict-access'><li>Strict Access</li></Link>
+          </ul>
         </nav>
 
         <Switch>
+          <Route path='/strict-access'>
+            <StrictAccess user={{ username: 'joaos', password: '1234' }} />
+          </Route>
 
           <Route path='/users/:id'>
             <Users greetingMessage='Good Morning' />
@@ -27,7 +33,6 @@ class App extends Component {
           <Route path='/'>
             <Home />
           </Route>
-
         </Switch>
       </BrowserRouter>
     );
--- exercises/02.frontend/block-13_life-cycles-components-and-react-router/D02_react-router/ex00_comprehension-exercises-router-master/src/StrictAcess.js
+++ exercises/02.frontend/block-13_life-cycles-components-and-react-router/D02_react-router/ex00_comprehension-exercises-router-master/src/StrictAcess.js
@@ -3,11 +3,12 @@ import { Redirect } from 'react-router-dom';
 
 class StrictAccess extends Component {
   render() {
+    console.log(this.props);
     const { username, password } = this.props.user;
     if (!(username === 'joao' && password === '1234')) {
       alert('Access denied');
       return <Redirect to="/" />
-    }
+    } else
     return (
       <p>Welcome joao!</p>
     );
