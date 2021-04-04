a7b58a9c49e3eeb9a3148bf36d661dac021f6c08
Murilo Gonçalves <murilo.gon@live.com>
2021-04-04 20:25:41 -0300

    feat(ex01-req01): :rocket: add / route to the application

--- exercises/02.frontend/block-13_life-cycles-components-and-react-router/D02_react-router/ex01_pokedex/src/App.js
+++ exercises/02.frontend/block-13_life-cycles-components-and-react-router/D02_react-router/ex01_pokedex/src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
+      <BrowserRouter>
+        <Switch>
+          <Route path='/'>
+            <Pokedex pokemons={pokemons} />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
