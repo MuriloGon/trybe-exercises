acefadb8690958ef6bab2910e1f1e1a5e1fe7935
Murilo Gonçalves <murilo.gon@live.com>
2021-04-05 14:47:05 -0300

    feat(ex04): 🚀 use filter and sort methods

--- exercises/01.fundamentals/B08_higher_order_functions/D03_map-and-filter/ex04.js
+++ exercises/01.fundamentals/B08_higher_order_functions/D03_map-and-filter/ex04.js
@@ -88,7 +88,10 @@ const expectedResult = [
 ];
 
 function oldBooks() {
-  // escreva seu código aqui
+  const currentYear = new Date().getFullYear();
+  return books
+    .filter(book => (currentYear - book.releaseYear) >= 60)
+    .sort((a, b) => a.releaseYear - b.releaseYear);
 }
 
 assert.deepStrictEqual(oldBooks(), expectedResult);
