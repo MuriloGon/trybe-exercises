3d75360a7d6c81eb8215bb37d4a9449075e5e26c
Murilo Gonçalves <murilo.gon@live.com>
2021-04-06 16:37:10 -0300

    feat(ex06): 🚀 use reduce and map(item, index) create obj

--- exercises/01.fundamentals/B08_higher_order_functions/D04_reduce/ex06.js
+++ exercises/01.fundamentals/B08_higher_order_functions/D04_reduce/ex06.js
@@ -4,7 +4,15 @@ const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
 const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
 function studentAverage() {
-  // escreva seu código aqui
+  if (students.length !== grades.length) throw Error('The arrays must\'ve the same length!!');
+
+  return students.map((student, index) => {
+    const average = grades[index].reduce((a, b) => a + b) / grades[index].length;
+    return {
+      name: student,
+      average: average,
+    }
+  })
 }
 
 const expected = [
