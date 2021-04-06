b3c5017ba3ab23f6993dd7aa5848a077db5d6e46
Murilo Gonçalves <murilo.gon@live.com>
2021-04-06 15:05:05 -0300

    feat(ex01): :rocket: use reduce with concat method

--- exercises/01.fundamentals/B08_higher_order_functions/D04_reduce/ex01.js
+++ exercises/01.fundamentals/B08_higher_order_functions/D04_reduce/ex01.js
@@ -8,7 +8,7 @@ const arrays = [
 ];
 
 function flatten() {
-  // escreva seu código aqui
+  return arrays.reduce((a, b) => a.concat(b), []);
 }
-
+console.log(flatten())
 assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
