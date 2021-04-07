a1b39bfad37fe94856f1d77847eedfe7c4602629
Murilo Gonçalves <murilo.gon@live.com>
2021-04-07 10:06:33 -0300

    feat(ex03): 🚀 use object destructuring

--- exercises/01.fundamentals/B08_higher_order_functions/D05_spread-operator_rest_descruturing_more/ex03.js
+++ exercises/01.fundamentals/B08_higher_order_functions/D05_spread-operator_rest_descruturing_more/ex03.js
@@ -15,7 +15,7 @@ const gunnar = {
 };
 
 // complete a assinatura da função abaixo
-const personLikes = () => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+const personLikes = ({ name, age, likes }) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
 
 assert.strictEqual(personLikes(alex), 'Alex is 26 years old and likes fly fishing.');
 assert.strictEqual(personLikes(gunnar), 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.');
