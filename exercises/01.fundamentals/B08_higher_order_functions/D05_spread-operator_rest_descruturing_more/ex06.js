b4268e405ce9d49eb7384d74c68943c65aa04c55
Murilo Gonçalves <murilo.gon@live.com>
2021-04-07 11:09:22 -0300

    feat(ex06): 🚀 use array destructuring and abbreviation object literal

--- exercises/01.fundamentals/B08_higher_order_functions/D05_spread-operator_rest_descruturing_more/ex06.js
+++ exercises/01.fundamentals/B08_higher_order_functions/D05_spread-operator_rest_descruturing_more/ex06.js
@@ -4,7 +4,7 @@ const palio = ['Palio', 'Fiat', 2019];
 const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
 const chiron = ['Chiron', 'Bugatti', 2016];
 
-// escreva toObject abaixo
+const toObject = ([name, brand, year]) => ({ name, brand, year });
 
 assert.deepStrictEqual(toObject(palio), { name: 'Palio', brand: 'Fiat', year: 2019 });
 assert.deepStrictEqual(toObject(shelbyCobra), { name: 'Shelby Cobra', brand: 'Ford', year: 1963 });
