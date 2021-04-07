b9d81d3facd2cc473fdd2e9937866554542964d5
Murilo Gonçalves <murilo.gon@live.com>
2021-04-07 10:12:54 -0300

    feat(ex04): 🚀 use object destructuring and filter

--- exercises/01.fundamentals/B08_higher_order_functions/D05_spread-operator_rest_descruturing_more/ex04.js
+++ exercises/01.fundamentals/B08_higher_order_functions/D05_spread-operator_rest_descruturing_more/ex04.js
@@ -28,9 +28,7 @@ const people = [
   },
 ];
 
-// escreva filterPeople abaixo
-
-const filteredPeople = filterPeople(people);
+const filteredPeople = people.filter(({ bornIn }) => bornIn < 2001);
 
 assert.deepStrictEqual(filteredPeople[0], { name: 'Nicole', bornIn: 1992, nationality: 'Australian' });
 assert.deepStrictEqual(filteredPeople[1], { name: 'Toby', bornIn: 1901, nationality: 'Australian' });
