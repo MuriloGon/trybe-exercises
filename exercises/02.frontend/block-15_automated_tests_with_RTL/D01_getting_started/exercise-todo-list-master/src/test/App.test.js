93bed1939634287efb30b6f6d1c4a4abff217968
Murilo Gonçalves <murilo.gon@live.com>
2021-04-09 17:09:06 -0300

    feat(ex01): :rocket: implement add listitem test

--- exercises/02.frontend/block-15_automated_tests_with_RTL/D01_getting_started/exercise-todo-list-master/src/test/App.test.js
+++ exercises/02.frontend/block-15_automated_tests_with_RTL/D01_getting_started/exercise-todo-list-master/src/test/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
 
 describe('Testando a aplicação, testando input', () => {
@@ -10,8 +10,40 @@ describe('Testando a aplicação, testando input', () => {
     expect(labelTask).toBeInTheDocument();
     expect(inputTask).toBeInTheDocument();
   });
-    
+
   test('Verificando o tipo do input', () => {
     expect(inputTask.type).toBe('text');
   });
+});
+
+describe('Exercício 1 - Testando o botão de adicionar tarefa', () => {
+  it('Existe um botão com o texto /Adicionar/?', () => {
+    const obj = render(<App />);
+    const btnSend = obj.getByText(/Adicionar/i);
+    expect(btnSend).toBeInTheDocument();
+  });
+
+  it('Verifica se o botão tem value /Adicionar/', () => {
+    const obj = render(<App />);
+    const btnSend = obj.getByText(/Adicionar/i);
+    expect(btnSend.value).toBe('Adicionar');
+  });
+
+  it('Verifica se a tarefa digitada é salva', () => {
+    const obj = render(<App />);
+
+    //Elements
+    const input = obj.container.querySelector('#inputTodo');
+    const addBtn = obj.container.querySelector('#btnAdd');
+
+    const listItem = 'Item da lista';
+
+    fireEvent.change(input, { target: { value: listItem } });
+
+    fireEvent.click(addBtn);
+    const listElement = obj.getByText(listItem);
+
+    expect(listElement).toBeInTheDocument();
+    expect(listElement).toHaveTextContent(listItem);
+  })
 });
