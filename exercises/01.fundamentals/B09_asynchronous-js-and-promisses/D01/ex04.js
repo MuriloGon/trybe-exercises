b0222b124e6e7215b5167f0fcc49e7957c05299b
Murilo Gonçalves <murilo.gon@live.com>
2021-04-12 17:58:33 -0300

    feat: :seedling: Initial files

---
+++ exercises/01.fundamentals/B09_asynchronous-js-and-promisses/D01/ex04.js
@@ -0,0 +1,11 @@
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+
+
+sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
