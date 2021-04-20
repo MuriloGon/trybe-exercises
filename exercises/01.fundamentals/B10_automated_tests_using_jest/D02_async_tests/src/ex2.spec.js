63e41a71d90149320cd8fd08ac92f38784f9e6d3
Murilo Gonçalves <murilo.gon@live.com>
2021-04-20 20:49:54 -0300

    feat(ex02): ✅ promisses test

---
+++ exercises/01.fundamentals/B10_automated_tests_using_jest/D02_async_tests/src/ex2.spec.js
@@ -0,0 +1,31 @@
+const { users, findUserById, getUserName } = require('./ex2');
+
+const randIndex = (arr) => Math.floor(Math.random() * arr.length);
+
+describe('EX2 - testins promisses', () => {
+  const existingIds = Object.keys(users);
+
+  test('existing ids', () => {
+    const idTested = existingIds[randIndex(existingIds)];
+    const nameExpected = users[idTested].name;
+
+    return getUserName(idTested)
+      .then((nameObj) => {
+        expect(nameObj).toBe(nameExpected)
+      });
+  });
+
+  test('non-existing ids', () => {
+    const idTested = 10;
+    const nameExpected = 'murilo';
+    const errorObj = { error: `User with ${idTested} not found.` }
+
+    return getUserName(idTested)
+      .then((nameObj) => {
+        expect(nameObj).toBe(nameExpected)
+      })
+      .catch((error) => {
+        expect(error).toEqual(errorObj);
+      });
+  });
+});
