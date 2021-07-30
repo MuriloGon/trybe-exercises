e40f8034d0015012ab968734d3e4784a6501934a
Murilo Gonçalves <murilo.gon@live.com>
2021-07-30 00:51:05 -0300

    feat(ex 7,8): :sparkles: implement std input for imc function

--- exercises/3.backend/block26-introduction-to-web-development-with-node-js/day1-node-js-introduction/my-scripts/imc.js
+++ exercises/3.backend/block26-introduction-to-web-development-with-node-js/day1-node-js-introduction/my-scripts/imc.js
@@ -1,5 +1,9 @@
+const { questionFloat } = require('readline-sync');
+
+
 function computeIMC(height, weight) {
   return weight / height ** 2;
 }
-
-console.log(computeIMC(1.35, 54));
+const height = questionFloat('Qual sua altura? (m):\t',)
+const weight = questionFloat('Qual seu peso? (kg):\t')
+console.log('Seu IMC é: ', computeIMC(height, weight));
