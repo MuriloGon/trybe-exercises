5019ef0673b398296d642d2ec322771016bbfe38
Murilo Gonçalves <murilo.gon@live.com>
2021-07-30 23:40:31 -0300

    feat(ex01): :rocket: finish exercise 01

---
+++ exercises/3.backend/block26-introduction-to-web-development-with-node-js/day2-async-flow/exercises/exercise01.js
@@ -0,0 +1,16 @@
+function nums(num1, num2, num3) {
+  return new Promise((resolve, reject) => {
+    if( num1 === typeof 'number' || num2 === typeof 'number' 
+      || num3 === typeof 'number') return reject(Error('Informe apenas números'));
+
+    const output = (num1 + num2) * num3;
+    if(output <= 50) return reject(Error('Valor muito baixo'));
+
+    resolve(output);
+  });
+}
+
+// nums(1, 22, 3).then( (data) => console.log('o resultado foi', data)).catch(e => console.error(e.message));
+// nums(1, '22', 3).then( (data) => console.log('o resultado foi', data)).catch(e => console.error(e.message));
+// nums(1, 22, 3).then( (data) => console.log('o resultado foi', data)).catch(e => console.error(e.message));
+export default nums;
