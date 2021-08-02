4e34faeb946f1fe20246e1cae092117b80eeffc9
Murilo Gonçalves <murilo.gon@live.com>
2021-08-02 03:39:10 -0300

    feat(ex06,07): implement GET /simpons/:id

--- exercises/03.backend/block26-introduction-to-web-development-with-node-js/day3-expres-http-with-nodejs/app-express/simpsons.js
+++ exercises/03.backend/block26-introduction-to-web-development-with-node-js/day3-expres-http-with-nodejs/app-express/simpsons.js
@@ -12,8 +12,16 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  let data;
+  try {
+    data = JSON.parse(await fs.readFile('./simpson.json', { encoding: 'utf-8' }));
+  } catch (error) { return res.status(500).send(error.message); };
+
+  const index = data.findIndex(({ id: idData }) => idData === id);
+  if (index >= 0) return res.status(200).json(data[index]);
+  return res.status(404).json({ message: 'Id não encontrado' })
 });
 
 module.exports = router;
