f57dc6931246b272ef8756ea4a37b7fb5b8039ea
Murilo Gonçalves <murilo.gon@live.com>
2021-08-02 02:45:20 -0300

    refactor: refactor GET /simpsons

--- exercises/03.backend/block26-introduction-to-web-development-with-node-js/day3-expres-http-with-nodejs/app-express/simpsons.js
+++ exercises/03.backend/block26-introduction-to-web-development-with-node-js/day3-expres-http-with-nodejs/app-express/simpsons.js
@@ -5,12 +5,15 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
   try {
-    const data = await fs.readFile('./simpson.json', { encoding: 'utf-8' })
-    return res.status(200).json(JSON.parse(data));
+    const data = JSON.parse(await fs.readFile('./simpson.json', { encoding: 'utf-8' }));
+    return res.status(200).json(data);
   } catch (error) {
-    console.log(error);
     return res.status(500).send(error.message);
   }
+});
+
+router.get('/:id', (req, res) => {
+  
 });
 
 module.exports = router;
