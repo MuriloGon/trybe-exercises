c9de8282e7b27fce614c7085e686a660331e2778
Murilo Gonçalves <murilo.gon@live.com>
2021-08-02 06:44:13 -0300

    feat(bonus02): implement POST /signup with auth token

--- exercises/03.backend/block26-introduction-to-web-development-with-node-js/day3-expres-http-with-nodejs/app-express/index.js
+++ exercises/03.backend/block26-introduction-to-web-development-with-node-js/day3-expres-http-with-nodejs/app-express/index.js
@@ -3,10 +3,22 @@ const rescue = require('express-rescue');
 const bodyParser = require('body-parser');
 const router = require('./simpsons');
 const authMiddleware = require('./authMiddleware');
+const generateToken = require('./generateToken');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+
+app.post('/signup', (req, res) => {
+  const { email, password, firstName, phone } = req.body;
+  if (!email || !password || !firstName || !phone) {
+    return res.status(401).json({ message: 'missing fields' });
+  }
+
+  res.status(200).json({ message: generateToken() });
+});
+
 app.use(authMiddleware);
 
 app.get('/ping', (_req, res) => {
