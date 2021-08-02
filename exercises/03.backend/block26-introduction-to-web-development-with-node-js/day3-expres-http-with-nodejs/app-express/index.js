487dfc4ce12bbb66df5a3b16f60e4bde2dc87f03
Murilo Gonçalves <murilo.gon@live.com>
2021-08-02 01:54:10 -0300

    feat(ex01): implement GET /ping

--- exercises/03.backend/block26-introduction-to-web-development-with-node-js/day3-expres-http-with-nodejs/app-express/index.js
+++ exercises/03.backend/block26-introduction-to-web-development-with-node-js/day3-expres-http-with-nodejs/app-express/index.js
@@ -5,7 +5,10 @@ const fs = require('fs/promises');
 
 const app = express();
 
-
+app.get('/ping', (req, res) => {
+  const message = 'pong';
+  res.status(200).json({ message });
+});
 
 app.listen(3000, () => {
   console.log('Running on port 3000 !!');
