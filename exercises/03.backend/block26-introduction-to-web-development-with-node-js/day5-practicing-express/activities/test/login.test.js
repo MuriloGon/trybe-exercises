7421d7d3cc936bc146863d76c0aea6e51cd858d6
Murilo Gonçalves <murilo.gon@live.com>
2021-08-04 00:18:04 -0300

    test: setup initial enviroment

---
+++ exercises/03.backend/block26-introduction-to-web-development-with-node-js/day5-practicing-express/activities/test/login.test.js
@@ -0,0 +1,8 @@
+import chai, {expect} from 'chai';
+import chaiHttp from 'chai-http';
+
+describe('atividade 01 - Rota /login', () => {
+  it('Deve receber email e senha', () => {
+    chai.request('localhost:3000');
+  });
+});
