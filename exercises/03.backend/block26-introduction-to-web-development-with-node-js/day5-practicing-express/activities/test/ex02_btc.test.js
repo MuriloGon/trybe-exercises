ff3fdd34fb11ba9f66ee2dff2b52d17be01d1884
Murilo Gonçalves <murilo.gon@live.com>
2021-08-04 05:10:13 -0300

    test(ex02): implement token req verification

---
+++ exercises/03.backend/block26-introduction-to-web-development-with-node-js/day5-practicing-express/activities/test/ex02_btc.test.js
@@ -0,0 +1,37 @@
+import chai, {expect} from 'chai';
+import app from '../index.js';
+import chaiHttp from 'chai-http';
+import Sinon from 'sinon';
+import crypto from 'crypto';
+chai.use(chaiHttp);
+
+const validToken = '1234abcd5678';
+const invalidToken1 = '123--12312-3';
+const invalidToken2 = '123412341234';
+const invalidToken3 = 'abcdABCDabcd';
+const invalidToken4 = '123asd';
+const invalidToken5 = '12345abcde12345';
+
+
+describe('atividade 01 - Rota /btc/price', () => {
+  it('verifica se o token recebido está no formato correto'+
+  ' retornando os o status equivalente para o erro ou sucesso', async () => {
+    const invalidCB = (req) => expect(req.status).to.be.eqls(400);
+    const validCB = (req) => expect(req.status).to.be.eqls(200);
+    await chai.request(app).post('/btc/price')
+        .send({token: invalidToken1}).then(invalidCB);
+    await chai.request(app).post('/btc/price')
+        .send({token: invalidToken2}).then(invalidCB);
+    await chai.request(app).post('/btc/price')
+        .send({token: invalidToken3}).then(invalidCB);
+    await chai.request(app).post('/btc/price')
+        .send({token: invalidToken4}).then(invalidCB);
+    await chai.request(app).post('/btc/price')
+        .send({token: invalidToken5}).then(invalidCB);
+    await chai.request(app).post('/btc/price')
+        .send({token: validToken}).then(validCB);
+  });
+  it('', () => {
+
+  });
+});
