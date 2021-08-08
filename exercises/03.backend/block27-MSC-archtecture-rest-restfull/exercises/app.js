4f004e25e0f46bc767a3fa90f7d22f95c6c2c94f
Murilo Gonçalves <murilo.gon@live.com>
2021-08-07 21:15:19 -0300

    feat(mongodb): finish mongo db implementations

--- exercises/03.backend/block27-MSC-archtecture-rest-restfull/exercises/app.js
+++ exercises/03.backend/block27-MSC-archtecture-rest-restfull/exercises/app.js
@@ -19,6 +19,39 @@ app.post('/user', createValidation, async (req, res) => {
   });
 });
 
+app.get('/user', async (_req, res) => {
+  const data = await User.getAll();
+  res.json(data);
+});
+
+app.get('/user/:id', async (req, res) => {
+  const { id } = req.params;
+  const data = await User.getById(id);
+  res.json(data);
+});
+
+app.put('/user/:id', createValidation, async (req, res) => {
+  const { id } = req.params;
+  const {
+    firstName, lastName, email, password,
+  } = req.body;
+
+  const matches = await User.updateById(id, firstName, lastName, email, password);
+  if (matches === null) return res.status(400).json({ error: true, message: 'id inválido' });
+
+  if (matches === 0) {
+    return res.status(404).json({ error: true, message: 'Usuário não encontrado' });
+  }
+
+  return res.json({
+    id,
+    firstName,
+    lastName,
+    email,
+    password,
+  });
+});
+
 app.listen(3000, () => {
   console.log('Tô listening a porta', 3000);
 });
--- exercises/03.backend/block27-MSC-archtecture-rest-restfull/exercises/models/mongodb/User.js
+++ exercises/03.backend/block27-MSC-archtecture-rest-restfull/exercises/models/mongodb/User.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require('mongodb');
 const connection = require('./connection');
 
 async function create(firstName, lastName, email, password) {
@@ -9,6 +10,45 @@ async function create(firstName, lastName, email, password) {
   return data;
 }
 
+async function getAll() {
+  const db = await connection();
+  const users = db.collection('users');
+  const data = await users.find().toArray();
+  return data;
+}
+
+async function getById(id) {
+  const db = await connection();
+  const users = db.collection('users');
+  const data = await users.find(
+    { _id: ObjectId(id) },
+    { projection: { _id: 0 } },
+  ).toArray();
+  return data;
+}
+
+async function updateById(id, firstName, lastName, email, password) {
+  const db = await connection();
+  const users = db.collection('users');
+
+  try {
+    const response = await users.updateOne(
+      { _id: ObjectId(id) },
+      {
+        $set: {
+          firstName, lastName, email, password,
+        },
+      },
+    );
+    return response.matchedCount;
+  } catch (error) {
+    return null;
+  }
+}
+
 module.exports = {
   create,
+  getAll,
+  getById,
+  updateById,
 };
