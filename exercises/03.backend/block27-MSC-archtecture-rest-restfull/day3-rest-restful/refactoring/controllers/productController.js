dc202c4c4428434aaa9767f921be8a4816171ad2
Murilo Gonçalves <murilo.gon@live.com>
2021-08-09 11:12:15 -0300

    feat(ex03): refactor http verbs to become semantically correct

--- exercises/03.backend/block27-MSC-archtecture-rest-restfull/day3-rest-restful/refactoring/controllers/productController.js
+++ exercises/03.backend/block27-MSC-archtecture-rest-restfull/day3-rest-restful/refactoring/controllers/productController.js
@@ -23,13 +23,13 @@ router.post('/add-user', async (req, res) => {
   res.json(newProduct);
 });
 
-router.post('/delete-user/:id', async (req, res) => {
+router.delete('/delete-user/:id', async (req, res) => {
   const products = await ProductModel.exclude(req.params.id);
 
   res.json(products);
 });
 
-router.post('/update-user/:id', async (req, res) => {
+router.put('/update-user/:id', async (req, res) => {
   const { name, brand } = req.body;
 
   const products = await ProductModel.update(req.params.id, name, brand);
