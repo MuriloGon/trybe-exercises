7d068e8dbf1275f6ba0635552fd37bada7da8e0a
Murilo Gonçalves <murilo.gon@live.com>
2021-08-19 17:09:51 -0300

    feat: add initial files

---
+++ exercises/03.backend/block28-authentication-and-files-upload/day3-integration tests/nodejs-jwt-base-project/api/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const apiRoutes = express.Router();
+apiRoutes.get('/api/posts', routes.getPosts);
+apiRoutes.post('/api/users', routes.createUsers);
+apiRoutes.post('/api/login', routes.login);
+
+app.use(apiRoutes);
+
+app.listen(port);
+console.log('conectado na porta ' + port);
